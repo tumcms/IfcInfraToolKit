5b39a876ca6620ab69a482a90f2b511976039f25
Sebastian Esser <sebastian.esser@tum.de>
2020-06-12 15:11:59 +0200

    added geometrygymIfc dll small updates in the ifc root code

--- IfcInfraToolkit/IfcInfraToolkit_Dyn/IFC.cs
+++ IfcInfraToolkit/IfcInfraToolkit_Dyn/IFC.cs
@@ -6,14 +6,7 @@
  */
 
 
-using System.Collections.Generic;
-using Autodesk.AECC.Interop.Land;
-using Autodesk.AECC.Interop.Roadway;
-using Autodesk.AECC.Interop.UiRoadway;
 using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GeometryGym.Ifc;
 
 namespace IfcInfraToolkit_Dyn
@@ -22,22 +15,29 @@ namespace IfcInfraToolkit_Dyn
     {
         private DatabaseIfc _vb;
 
-        //Construktor
-        public IFC_root(String FamilyName, String first_name,String organzization)
+        //Constructor
+        public IFC_root(string familyName, string firstName, string organization)
         {
+            // init new Ifc model database
             _vb = new DatabaseIfc(ModelView.Ifc4Reference);
             _vb.Release = ReleaseVersion.IFC4X3;
+
+            // set model author and organization
             IfcPerson author = new IfcPerson(_vb);
-            author.FamilyName = FamilyName;
-            author.GivenName = first_name;
-            IfcOrganization org = new IfcOrganization(_vb,organzization);
+            author.FamilyName = familyName;
+            author.GivenName = firstName;
+            IfcOrganization org = new IfcOrganization(_vb, organization);
 
         }
 
-        //Finalising 
-        public void IFC_export(String filepath)
+        //Finalizing 
+        public void IFC_export(string modelName, string filepath)
         {
-            _vb.WriteFile(filepath);
+            // build path
+            var finalPath = filepath + "/" + modelName + ".ifc";
+
+            // write the database to the STEP file
+            _vb.WriteFile(finalPath);
         }
 
     }
