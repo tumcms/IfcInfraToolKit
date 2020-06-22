d555c428ab74b0c89b557f62efa475cbcc7e7ae8
h_stefan <stefan.1995.huber@tum.de>
2020-06-22 09:48:39 +0200

    Added profiles to ifc.cs

--- IfcInfraToolkit/IfcInfraToolkit_Dyn/IFC.cs
+++ IfcInfraToolkit/IfcInfraToolkit_Dyn/IFC.cs
@@ -8,6 +8,8 @@
 
 using System;
 using GeometryGym.Ifc;
+using System.Collections.Generic;
+using Autodesk.DesignScript.Geometry;
 
 namespace IfcInfraToolkit_Dyn
 {
@@ -39,6 +41,61 @@ namespace IfcInfraToolkit_Dyn
             // write the database to the STEP file
             _vb.WriteFile(finalPath);
         }
+
+        //add Roadcrosssection 
+        //still requires testing
+        public IFC_root IFC_road_add(List<double> stations,List<double> width_left, List<double> width_right, List<double> slope_left,List<double> slope_right)
+        {
+            //Create dummy curve ONLY FOR TESTING need to be changed later 
+            var curve = new IfcAlignmentCurve(_vb);
+
+            //Create lists in which the widths/slopes are sorted in tuple
+            //ordered by station
+            List<List<double>> widths = new List<List<double>>();
+            List<List<double>> slopes = new List<List<double>>();
+
+            //List for all Crossprofiles and DistanceExpression
+            List<IfcOpenCrossProfileDef> ocpd = new List<IfcOpenCrossProfileDef>();
+            List<IfcDistanceExpression> de = new List<IfcDistanceExpression>();
+
+            //Create site for export
+            IfcSite site = new IfcSite(_vb, "locel_site");
+            
+
+            for (int i = 0; i < width_right.Count; i++)
+            {
+                //Add the widths at each Station to the final list
+                widths[i].Add(width_left[i]);
+                widths[i].Add(width_right[i]);
+
+                //Add the slopes at each Station to the final list
+                slopes[i].Add(width_left[i]);
+                slopes[i].Add(width_right[i]);
+
+            }
+
+            //Add at each station the Crossprofiledef
+            for (int i = 0; i< widths.Count; i++) {
+
+                //Create OpenCrossProfileDef and collect them
+                IfcOpenCrossProfileDef crsec = new IfcOpenCrossProfileDef(_vb, "Road_part " + (i + 1),true, widths[i], slopes[i]);
+                ocpd.Add(crsec);
+                //Create DistanceExpression and collect them
+                IfcDistanceExpression distex = new IfcDistanceExpression(_vb, stations[i]);
+                de.Add(distex);
+
+            }
+
+            
+            //link OpenCrossProfileDef to DistanceExpression
+            IfcSectionedSurface test = new IfcSectionedSurface(curve, de, ocpd, true);
+
+            
+
+            return this;
+        }
+
+
 
     }
 }
