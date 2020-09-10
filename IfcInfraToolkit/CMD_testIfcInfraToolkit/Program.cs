2e4797482168ca5593dee98807341e32b8157fcf
Sebastian_Esser@TUBVCMS-SES-3-W <sebastian.esser@tum.de>
2020-09-10 14:33:58 +0200

    started some demo cmd app

---
+++ IfcInfraToolkit/CMD_testIfcInfraToolkit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IfcInfraToolkit;
+using GeometryGym.Ifc;
+
+namespace CMD_testIfcInfraToolkit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myProjectSetupService = new IfcInfraToolkit.ProjectSetupService();
+            var myIfcDatabase = new GeometryGym.Ifc.DatabaseIfc();
+
+            myIfcDatabase = myProjectSetupService.AddBaseProjectSetup(myIfcDatabase);
+
+
+            myIfcDatabase.WriteFile("myIfcModel.ifc");
+        }
+    }
+}
---
+++ IfcInfraToolkit/IfcInfraToolkit/ProjectSetupService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryGym.Ifc;
+
+namespace IfcInfraToolkit
+{
+    public class ProjectSetupService
+    {
+        public ProjectSetupService()
+        {
+
+        }
+
+        public GeometryGym.Ifc.DatabaseIfc AddBaseProjectSetup(DatabaseIfc database)
+        {
+            var project = new IfcProject(database, null);
+
+            return database;
+        }
+
+
+    }
+}
