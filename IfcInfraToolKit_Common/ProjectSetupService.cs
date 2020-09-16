d1d8c4ccfc01d790cfcfb047957fe4e7e8830d70
Sebastian_Esser@TUBVCMS-SES-3-W <sebastian.esser@tum.de>
2020-09-16 14:27:23 +0200

    adding facilities is up and running

--- IfcInfraToolKit_Common/ProjectSetupService.cs
+++ IfcInfraToolKit_Common/ProjectSetupService.cs
@@ -47,18 +47,23 @@ namespace IfcInfraToolkit_Common
         /// </summary>
         /// <param name="database"></param>
         /// <param name="FacilityName"></param>
-        /// <param name="FacilityType"></param>
         /// <param name="host"></param>
         /// <returns></returns>
-        public DatabaseIfc AddFacility(DatabaseIfc database, string FacilityName, string FacilityType,
-            IfcSpatialStructureElement host)
+        public Guid AddFacility(ref DatabaseIfc database, string FacilityName, IfcSpatialStructureElement host)
         {
+            if (host == null)
+            {
+                var project = database.Project;
+                host = project.Extract<IfcSite>().First();
+            }
+
+
             // create an IfcFacility element
             var trafficFacility = new IfcFacility(host, FacilityName)
             {
                 CompositionType = IfcElementCompositionEnum.COMPLEX
             };
-            return database;
+            return trafficFacility.Guid;
         }
 
         /// <summary>
@@ -69,16 +74,16 @@ namespace IfcInfraToolkit_Common
         /// <param name="facilityType"></param>
         /// <param name="host"></param>
         /// <returns></returns>
-        public DatabaseIfc AddFacilityPart(DatabaseIfc database, string facilityPartName, string facilityType,
-            IfcFacility host)
+        public Guid AddFacilityPart(ref DatabaseIfc database, string facilityPartName, string facilityType,
+            IfcSpatialStructureElement host)
         {
             // ToDo: parse user input and select corresponding IFC content
             var partType = new IfcFacilityPartTypeSelect(IfcBridgePartTypeEnum.ABUTMENT);
             var usage = IfcFacilityUsageEnum.LATERAL;
 
-            var facilityPart = new IfcFacilityPart(host, facilityPartName, partType, usage);
+            var facilityPart = host.StepClassName == "IfcFacility" ? new IfcFacilityPart(host as IfcFacility, facilityPartName, partType, usage) : new IfcFacilityPart(host as IfcFacilityPart, facilityPartName, partType, usage);
 
-            return database;
+            return facilityPart.Guid;
         }
     }
 }
--- IfcInfraToolKit_DynamoCore/IfcModelSetup.cs
+++ IfcInfraToolKit_DynamoCore/IfcModelSetup.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Autodesk.DesignScript.Runtime;
+using GeometryGym.Ifc;
 using IfcInfraToolkit_Common;
 
 
@@ -10,6 +13,9 @@ namespace IfcInfraToolKit_DynamoCore
     /// </summary>
     public static class IfcModelSetup 
     {
+        #region CreateAndSave
+
+        
         /// <summary> Creates a new DatabaseIfc instance that acts as a central container for the IFC content. </summary>
         /// <search> init, create, IFC </search>
         /// <returns> DatabaseContainer that owns the DatabaseIfc object of GeometryGymIfc </returns>
@@ -22,7 +28,8 @@ namespace IfcInfraToolKit_DynamoCore
             // add content to the database
             ProjectSetupService service = new ProjectSetupService();
             container.Database = service.CreateDatabase();
-            
+            container.Database = service.AddBaseProjectSetup(container.Database);
+
             // beautiful return values
             var d = new Dictionary<string, object>
             {
@@ -31,7 +38,7 @@ namespace IfcInfraToolKit_DynamoCore
 
             return d;
         }
-
+        
         /// <summary> Stores an DatabaseIfc instance into an *.ifc Model </summary>
         /// <param name="container">IFC container</param>
         /// <param name="path">folder to store the resulting model</param>
@@ -41,6 +48,96 @@ namespace IfcInfraToolKit_DynamoCore
         {
             container.Database.WriteFile(path + "/" + modelName + ".ifc");
         }
+
+        #endregion
+
+        #region SpatialStructure
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="databaseContainer"></param>
+        /// <param name="facilityName"></param>
+        /// <param name="hostGuid">GlobalId of parent element</param>
+        /// <returns></returns>
+        [MultiReturn(new[] { "DatabaseContainer", "FacilityGUID" })]
+        public static Dictionary<string, object> AddFacility(DatabaseContainer databaseContainer, string facilityName, string hostGuid = "null")
+        {
+            // get current db
+            var database = databaseContainer.Database;
+
+            // get host
+            var host = database.OfType<IfcSpatialStructureElement>().FirstOrDefault(a => a.Guid.ToString() == hostGuid);
+
+            ProjectSetupService service = new ProjectSetupService();
+            var guid = service.AddFacility(ref database, facilityName, host);
+
+            // assign updated db to container
+            databaseContainer.Database = database;
+
+            // beautiful return values
+            var d = new Dictionary<string, object>
+            {
+                {"DatabaseContainer", databaseContainer},
+                {"FacilityGUID", guid.ToString()}
+            };
+
+            return d;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="databaseContainer"></param>
+        /// <param name="facilityPartName"></param>
+        /// <param name="hostGUID"></param>
+        /// <returns></returns>
+        [MultiReturn(new[] { "DatabaseContainer", "FacilityPartGUID" })]
+        public static Dictionary<string, object> AddFacilityPart(DatabaseContainer databaseContainer, string facilityPartName, string hostGUID)
+        {
+            // get current db
+            var database = databaseContainer.Database;
+
+            // get host
+            var hostFacility = database.OfType<IfcFacility>().First(a => a.Guid.ToString() == hostGUID);
+            var hostFacilityPart = database.OfType<IfcFacilityPart>().First(a => a.Guid.ToString() == hostGUID);
+
+            ProjectSetupService service = new ProjectSetupService();
+
+            Guid guid; 
+
+            if (hostFacility != null)
+            {
+                var host = hostFacility;
+                guid = service.AddFacilityPart(ref database, facilityPartName, "type", host);
+            }
+            else if (hostFacilityPart != null)
+            {
+                var host = hostFacilityPart;
+                guid = service.AddFacilityPart(ref database, facilityPartName, "type", host);
+            }
+            else
+            {
+                guid = Guid.Empty;
+            }
+           
+
+            // assign updated db to container
+            databaseContainer.Database = database;
+            // beautiful return values
+            var d = new Dictionary<string, object>
+            {
+                {"DatabaseContainer", databaseContainer},
+                {"FacilityPartGUID", guid.ToString()}
+            };
+
+            return d;
+        }
+
+
+
+
+        #endregion
 
     }
 }
