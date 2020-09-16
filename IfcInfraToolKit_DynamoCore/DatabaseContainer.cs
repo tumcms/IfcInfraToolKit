d0c72e1cf0beaa7f8a5792f9a0301a293c9da4d1
Sebastian_Esser@TUBVCMS-SES-3-W <sebastian.esser@tum.de>
2020-09-16 12:15:14 +0200

    introducing a database container, basic create and write sample in the core section

---
+++ IfcInfraToolKit_DynamoCore/DatabaseContainer.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.DesignScript.Runtime;
+using GeometryGym.Ifc;
+
+namespace IfcInfraToolKit_DynamoCore
+{
+    [IsVisibleInDynamoLibrary(false)]   // Don't show this class in Dynamo but make it accessible among all dlls
+    public class DatabaseContainer
+    {
+        public DatabaseIfc Database { get; set; }
+        public string ExportPath { get; set; }
+        public string ModelName { get; set; }
+    }
+}
--- IfcInfraToolKit_DynamoCore/IfcModelSetup.cs
+++ IfcInfraToolKit_DynamoCore/IfcModelSetup.cs
@@ -1,16 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using IfcInfraToolKit;
+﻿using System.Collections.Generic;
+using Autodesk.DesignScript.Runtime;
+using IfcInfraToolkit_Common;
+
 
 namespace IfcInfraToolKit_DynamoCore
 {
-    public class IfcModelSetup
+    /// <summary>
+    /// Provides basic methods to create an IFC model and add some spatial structure stuff
+    /// </summary>
+    public static class IfcModelSetup 
     {
+        /// <summary> Creates a new DatabaseIfc instance that acts as a central container for the IFC content. </summary>
+        /// <search> init, create, IFC </search>
+        /// <returns> DatabaseContainer that owns the DatabaseIfc object of GeometryGymIfc </returns>
+        [MultiReturn(new[] {"DatabaseContainer"})]
+        public static Dictionary<string, object> CreateIfcModel()
+        {
+            // init container
+            var container = new DatabaseContainer();
 
+            // add content to the database
+            ProjectSetupService service = new ProjectSetupService();
+            container.Database = service.CreateDatabase();
+            
+            // beautiful return values
+            var d = new Dictionary<string, object>
+            {
+                {"DatabaseContainer", container}
+            };
 
+            return d;
+        }
+
+        /// <summary> Stores an DatabaseIfc instance into an *.ifc Model </summary>
+        /// <param name="container">IFC container</param>
+        /// <param name="path">folder to store the resulting model</param>
+        /// <param name="modelName">file name without *.ifc label</param>
+        /// <search> store, save, IFC </search>
+        public static void SaveIfcModel(DatabaseContainer container, string path, string modelName)
+        {
+            container.Database.WriteFile(path + "/" + modelName + ".ifc");
+        }
 
     }
 }
