ea414affadb49dbaf9698f283e429a95ef8743c5
Sebastian_Esser <sebastian.esser@tum.de>
2020-11-02 14:41:05 +0100

    started alignment importer based on csv file

---
+++ IfcInfraToolKit_DynamoCore/AlignmentImporter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Eventing;
+using System.Runtime.InteropServices;
+using Alignment_ImportService;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Runtime;
+using Off_GeomLibrary;
+
+namespace IfcInfraToolKit_DynamoCore
+{
+    public class AlignmentImporter
+    {
+        public AlignmentImporter()
+        {
+
+        }
+
+        /// <summary> visualizing Off Geometry </summary>
+        /// <search> off, pointcloud </search>
+        /// <returns>  </returns>
+        [MultiReturn(new[] { "DynamoPointList" })]
+        public static Dictionary<string, object> ImportAlignmentAsPoints(string csvPath)
+        {
+            var importService = new AlignmentImportService();
+            importService.ImportFromCSV(csvPath);
+
+            var ptList = new List<Autodesk.DesignScript.Geometry.Point>();
+
+            foreach (var pt in importService.points)
+            {
+                Autodesk.DesignScript.Geometry.Point p = Point.ByCoordinates(pt.X, pt.Y, pt.Z);
+                ptList.Add(p);
+            }
+
+            // beautiful return values
+            var d = new Dictionary<string, object>
+            {
+                {"DynamoPointList", ptList}
+            };
+
+            return d;
+
+        }
+    }
+}
