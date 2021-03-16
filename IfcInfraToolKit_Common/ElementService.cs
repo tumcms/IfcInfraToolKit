ed2f93bb5b3c8667beafc1050ad5ba0be5d29994
Sebastian_Esser <sebastian.esser@tum.de>
2021-03-16 15:58:52 +0100

    New content to element designer

--- IfcInfraToolKit_Common/ElementService.cs
+++ IfcInfraToolKit_Common/ElementService.cs
@@ -1,10 +1,47 @@
 ﻿
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GeometryGym.Ifc;
+
 namespace IfcInfraToolkit_Common
 {
     public class ElementService
     {
-        // ToDo: Implement methods to create (almost) any type of IfcElement
+       
+        // ToDo: Create method overloading to accept also geometry and placement
+
+        public string AddElement(ref DatabaseIfc database, string elementName, string IfcClass, string hostGuid)
+        {
+            // get host
+            var host = database.OfType<IfcObjectDefinition>().FirstOrDefault(a => a.Guid.ToString() == hostGuid);
+
+            // catch issues of no element with specified host guid is available
+            if (host== null)
+            {
+                host = database.Project.UppermostSite();
+            }
+
+            // ToDo: extend the dict to create any kind of IfcElement specialization
+            // ToDo: catch exceptions of specific IfcClass doesnt exist
+            Dictionary<string, Func<IfcElement>> dict = new Dictionary<string, Func<IfcElement>>
+            {
+                {"IfcBeam", () => new IfcBeam(host, null, null)},
+                {"IfcBearing", () => new IfcBearing(host, null, null)},
+                {"IfcBuiltElement", () => new IfcBuiltElement(host, null, null)},
+                {"IfcWall", () => new IfcWall(host, null, null)}
+            };
+
+            
+
+            var buildingElement = dict[IfcClass]();
+            buildingElement.Name = elementName;
+
+            // return GUID of recently created element
+            return buildingElement.GlobalId;
+        }
+
 
     }
 }
---
+++ IfcInfraToolKit_DynamoCore/ElementDesigner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autodesk.DesignScript.Runtime;
+using GeometryGym.Ifc;
+using IfcInfraToolkit_Common;
+
+namespace IfcInfraToolKit_DynamoCore
+{
+    public class ElementDesigner
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="databaseContainer"></param>
+        /// <param name="IfcClass">Desired IfcClass name</param>
+        /// <param name="elementName"></param>
+        /// <param name="hostGuid">GlobalId of parent element</param>
+        /// <returns></returns>
+        [MultiReturn(new[] { "DatabaseContainer", "ElementGUID" })]
+        public static Dictionary<string, object> AddElement(DatabaseContainer databaseContainer,
+            string IfcClass, string elementName = "DefaultElement",
+            string hostGuid = "null")
+        {
+            // get current db
+            var database = databaseContainer.Database;
+            
+            var service = new ElementService();
+            var guid = service.AddElement(ref database, elementName, IfcClass, hostGuid);
+
+            // assign updated db to container
+            databaseContainer.Database = database;
+
+            // beautiful return values
+            var d = new Dictionary<string, object>
+            {
+                {"DatabaseContainer", databaseContainer},
+                {"ElementGUID", guid}
+            };
+
+            return d;
+        }
+
+    }
+}
