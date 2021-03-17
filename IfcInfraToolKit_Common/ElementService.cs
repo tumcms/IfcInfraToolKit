dd01589059bf5158e0d182348820e886a7966d0e
ga45fat <ga45fat@mytum.de>
2021-03-17 16:53:12 +0100

    Specification for IfcElement added (not yet complete)

--- IfcInfraToolKit_Common/ElementService.cs
+++ IfcInfraToolKit_Common/ElementService.cs
@@ -9,9 +9,6 @@ namespace IfcInfraToolkit_Common
 {
     public class ElementService
     {
-       
-        // ToDo: Create method overloading to accept also geometry and placement
-
         public string AddElement(ref DatabaseIfc database, string elementName, string IfcClass, string hostGuid, IfcProductDefinitionShape representation=null, IfcLocalPlacement placement=null)
         {
             // get host
@@ -25,16 +22,93 @@ namespace IfcInfraToolkit_Common
 
             // ToDo: extend the dict to create any kind of IfcElement specialization
             // ToDo: catch exceptions of specific IfcClass doesnt exist
+            //For all subclasses of IfcDistributionElements another input parameter is necessary (IfcDistributionSystem)
+            //Not yet completed!!!
             Dictionary<string, Func<IfcElement>> dict = new Dictionary<string, Func<IfcElement>>
             {
+                //Contains all subclasses of IfcBuiltElement
                 {"IfcBeam", () => new IfcBeam(host, null, null)},
                 {"IfcBearing", () => new IfcBearing(host, null, null)},
+                {"IfcBuildingElementProxy", () => new IfcBuildingElementProxy(host, null, null) },
                 {"IfcBuiltElement", () => new IfcBuiltElement(host, null, null)},
-                {"IfcWall", () => new IfcWall(host, null, null)}
+                {"IfcChimney", () => new IfcChimney(host,null,null) },
+                {"IfcColumn", () => new IfcColumn(host,null,null) },
+                {"IfcCourse", () => new IfcCourse(host,null,null) },
+                {"IfcCovering", () => new IfcCovering(host,null,null) },
+                {"IfcCurtainWall", () => new IfcCurtainWall(host,null,null) },
+                {"IfcDeepFoundation", () => new IfcDeepFoundation(host,null,null) },
+                {"IfcDoor", () => new IfcDoor(host,null,null) },
+                {"IfcEarthworksElement", () => new IfcEarthworksElement(host,null,null) },
+                {"IfcFooting", () => new IfcFooting(host,null,null) },
+                {"IfcKerb", () => new IfcKerb(host,null,null) },
+                {"IfcMember", () => new IfcMember(host,null,null) },
+                {"IfcMooringDevice", () => new IfcMooringDevice(host,null,null) },
+                {"IfcNavigationElement", () => new IfcNavigationElement(host,null,null) },
+                {"IfcPavement", () => new IfcPavement(host,null,null) },
+                {"IfcPlate", () => new IfcPlate(host,null,null) },
+                {"IfcRail", () => new IfcRail(host,null,null) },
+                {"IfcRailing", () => new IfcRailing(host,null,null) },
+                {"IfcRamp", () => new IfcRamp(host,null,null) },
+                {"IfcRampFlight", () => new IfcRampFlight(host,null,null) },
+                {"IfcRoof", () => new IfcRoof(host,null,null) },
+                {"IfcShadingDevice", () => new IfcShadingDevice(host,null,null) },
+                {"IfcSlab", () => new IfcSlab(host,null,null) },
+                {"IfcStair", () => new IfcStair(host,null,null) },
+                {"IfcStairFlight", () => new IfcStairFlight(host,null,null) },
+                {"IfcTrackElement", () => new IfcTrackElement(host,null,null) },
+                {"IfcWall", () => new IfcWall(host, null, null)},
+                {"IfcWindow", () => new IfcWindow(host,null,null) },
+                //IfcCivilElement
+                {"IfcCivilElement", () => new IfcCivilElement(host,null,null) },
+                //IfcDistributionElement
+                {"IfcDistributionElement", () => new IfcDistributionElement(host,null,null) },
+                {"IfcDistributionControlElement", () => new IfcDistributionControlElement(host,null,null,null) },
+                {"IfcActuator", () => new IfcActuator(host,null,null, null) },
+                {"IfcAlarm", () => new IfcAlarm(host,null,null, null) },
+                {"IfcController", () => new IfcController(host,null,null, null) },
+                {"IfcFlowInstrument", () => new IfcFlowInstrument(host,null,null, null) },
+                {"IfcProtectiveDeviceTrippingUnit", () => new IfcProtectiveDeviceTrippingUnit(host,null,null, null) },
+                {"IfcSensor", () => new IfcSensor(host,null,null, null) },
+                {"IfcUnitaryControlElement", () => new IfcUnitaryControlElement(host,null,null, null) },
+                {"IfcDistributionFlowElement", () => new IfcDistributionFlowElement(host,null,null, null) },
+                {"IfcDistributionChamberElement", () => new IfcDistributionChamberElement(host,null,null, null) },
+                {"IfcEnergyConversionDevice", () => new IfcEnergyConversionDevice(host,null,null, null) },
+                {"IfcFlowController", () => new IfcFlowController(host,null,null, null) },
+                {"IfcFlowFitting", () => new IfcFlowFitting(host,null,null, null) },
+                {"IfcFlowMovingDevice", () => new IfcFlowMovingDevice(host,null,null, null) },
+                {"IfcFlowSegment", () => new IfcFlowSegment(host,null,null, null) },
+                {"IfcFlowStorageDevice", () => new IfcFlowStorageDevice(host,null,null, null) },
+                {"IfcFlowTerminal", () => new IfcFlowTerminal(host,null,null, null) },
+                {"IfcFlowTreatmentDevice", () => new IfcFlowTreatmentDevice(host,null,null, null) },
+                //For IfcElementAssembly two different attributes are needed
+                //{"IfcElementAssembly", () => new IfcElementAssembly(host,null,null) },
+
+                //IfcElementComponent
+                {"IfcBuildingElementPart", () => new IfcBuildingElementPart(host,null,null) },
+                {"IfcDiscreteAccessory", () => new IfcDiscreteAccessory(host,null,null) },
+                {"IfcFastener", () => new IfcFastener(host,null,null) },
+                {"IfcImpactProtectionDevice", () => new IfcImpactProtectionDevice(host,null,null) },
+                {"IfcMechanicalFastener", () => new IfcMechanicalFastener(host,null,null) },
+                //IfcReinforcingElement is abstract
+                {"IfcReinforcingBar", () => new IfcReinforcingBar(host,null,null) },
+                {"IfcReinforcingMesh", () => new IfcReinforcingMesh(host,null,null) },
+                {"IfcTendon", () => new IfcTendon(host,null,null) },
+                {"IfcTendonAnchor", () => new IfcTendonAnchor(host,null,null) },
+                {"IfcTendonConduit", () => new IfcTendonConduit(host,null,null) },
+                {"IfcSign", () => new IfcSign(host,null,null) },
+                {"IfcVibrationDamper", () => new IfcVibrationDamper(host,null,null) },
+                {"IfcVibrationIsolator", () => new IfcVibrationIsolator(host,null,null) },
+
+                //..........
+                {"Default", () => new IfcBuildingElementProxy(host,null,null) },
+
             };
 
+            //ToDo: Catch Exception if Element does not exist?
+            //Choose correct IfcElement class from dictionary
+            var buildingElement = dict.ContainsKey(IfcClass) ? dict[IfcClass]() : dict["Default"]();
+
             //set attributes of the building element
-            var buildingElement = dict[IfcClass]();
             buildingElement.Name = elementName;
             buildingElement.Representation = representation;
             buildingElement.ObjectPlacement = placement;
